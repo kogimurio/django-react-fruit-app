21eb318099c9580f68816eb4bb56b496cd742fd9
unknown <kogimurio@gmail.com>
2024-09-24 02:31:06 -0700

    secong commit

--- Product/urls.py
+++ Product/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('productlist/', views.product_list)
+    path('productlist/', views.product_list),
+    path('productdetail/<int:pk>/', views.product_detail),
 ]
--- Product/views.py
+++ Product/views.py
@@ -20,3 +20,24 @@ def product_list(request):
             return Response(serialiser.data, status=status.HTTP_201_CREATED)
         return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+    
+    if request.method == 'GET':
+        serialiser = ProductSerializer(product)
+        return Response(serialiser.data)
+    
+    if request.method == 'PUT':
+        serialiser = ProductSerializer(product, data=request.data)
+        if serialiser.is_valid():
+            serialiser.save()
+            return Response(serialiser.data)
+        return Response(serialiser.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    if request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
